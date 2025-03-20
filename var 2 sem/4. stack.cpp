6e461ceebe972b209ec51d28892e4d2e76924637
DaSe-frog <63409744+DaSe-frog@users.noreply.github.com>
2025-03-20 09:17:11 +0300

    Update 4. stack.cpp

--- var 2 sem/4. stack.cpp
+++ var 2 sem/4. stack.cpp
@@ -24,28 +24,17 @@ Stack* s_push(Stack* p, int in){	// указатель на вершину и в
 	t->data = in;				// формирование информационной части
 	t->next = p;				// формирование адресной части
 	return t;					// возвращаем измененное значение вершины
-	// если хотим из основной функции добавить в стек новый элемент а, при этом вершиной стека является указатель бегин, то:
-	// begin = s_push(begin,a)
 }
 
 //	прсомотр стека
 void s_view(Stack* p) {
 	Stack* t = p;
-	while (t != NULL) {		//	пока список не окончился
+	while (t != NULL) {		
 		cout << " " << t->data << endl;
-		t = t->next;		//	текущий указатель переставляем на следующий элемент, адрес которого хранится в поле next текущего элемента
+		t = t->next;
 	}
 }
-// получение информации из вершины стека с извлечением
-Stack* OutStack(Stack* p, int* out) {
-	Stack* t = p;			// устанавливаем указатель т на веришну п
-	*out = p->data;			//	
-	p = p->next;			//	переставляем вершину п на следующий элемент
-	delete t;				//	удаляем бывшую вершину т
-	return p;				// возвращаем новую вершину п
 
-	// обращение к этой функции begin=OutStack(begin,&a);
-}
 
 // функция освобождения памяти, занятой стеком
 void s_del(Stack** p) {
@@ -55,22 +44,23 @@ void s_del(Stack** p) {
 		*p = (*p)->next;
 		delete t;
 	}
-	
-	// обращение к этой функции: s_del(&begin) после выполнения которой указатель на вершину begin будет равен NULL
 }
 
-
-void stack_sort3(Stack** p) {
+void StackSort(Stack* p) {
 	Stack* t = NULL;
-	int temp_stack;
-	for ((*p)->data; (*p)->next != NULL; (*p) = (*p)->next) {
-		if ((*p)->data > (*p)->next->data) {
-			OutStack(*p, &(*p)->next->data);
-		}
-		
-	}
-	s_view(*p);
+	Stack* tl;
+	int r, i = 0;
+	do {
+		for (tl = p; tl->next != t; tl = tl->next) {
+			if (tl->data > tl->next->data) {
+				r = tl->data;
+				tl->data = tl->next->data;
+				tl->next->data = r;
+			}
+		}t = tl;
+	} while (p->next != t);
 }
+
 
 int main() {
 	Stack* begin = NULL;
@@ -80,6 +70,7 @@ int main() {
 		cout << "\n\t1. Create\n\t2. Add\n\t3. View\n\t4. Delete\n\t5. Individual task\n\t6. Sorting\n\t0. Exit\n\t";
 		cin >> kod;
 		switch (kod) {
+		
 		case 1: case 2:								//create , add
 			cout << "Input the num of elements: ";
 			cin >> n;
@@ -90,6 +81,7 @@ int main() {
 			if (kod == 1)cout << "Create " << n << " elements." << endl;
 			else  cout << "Add " << n << " elements." << endl;
 			break;
+		
 		case 3:										// view
 			if (!begin) {
 				cout << "The Stack is empty!" << endl;
@@ -97,13 +89,15 @@ int main() {
 			}
 			s_view(begin);
 			break;
+		
 		case 4:										//delete
 			s_del(&begin);
 			cout << "The memory is free!" << endl;
 			break;
-			
-		case 5:			
-			while (begin  != NULL) { 
+
+		
+		case 5:
+			while (begin != NULL) {
 				if (begin->data % 5 == 0) {
 					if (begin->data % 10 != 0)	cout << "Delete " << begin->data << endl;
 					else temp_begin = s_push(temp_begin, begin->data);
@@ -117,22 +111,20 @@ int main() {
 				temp_begin = temp_begin->next;
 			}
 			delete temp_begin;
+			cout << "\tNew stack: " << endl;
+			s_view(begin);
 			break;
-			//доделать  6
+		
 		case 6:										// сортировка
-			cout << "1" << endl;
+			StackSort(begin);
+			cout << "\tNew stack: " << endl;
 			s_view(begin);
-			stack_sort3(&begin);
-			cout << "2 after _sort3" << endl;
-			s_view(begin);
-			//}
-
-			// обращение к этой функции: s_del(&begin) после выполнения которой указатель на вершину begin будет равен NULL
-		//}
 			break;
+		
 		case 0:
 			if (begin != NULL) s_del(&begin);
 			exit(0);
+		
 		default:
 			cout << "The answer doesn't exist. Try again!";
 			break;
